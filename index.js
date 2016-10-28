6dbd2fbfd13a0a742b00b2b23fe0511419deeb6f
pi0ner <pi0ner@inbox.ru>
2016-10-29 01:29:39 +0300

    [#3][+]file input/output

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 
 var path = require("path");
 
-//TODO: refactoring(много слов)
+//config jsons
 var moduleConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'obfuscatorConfig.json'), 'utf8'));
 var userConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'userConfig.json'), 'utf8'));
 
@@ -16,17 +16,32 @@ var migrationConfig = moduleConfig.migration;
 //User config
 var inputOutputWords = userConfig.inputOutputWords
 var sqlDialect = moduleConfig.postgres;
+var dbSetting = userConfig.dbConfig;
+
 var inputFilename= userConfig.environment.inputFilename;
 var outputFilename = userConfig.environment.outputFilename ?
     userConfig.environment.outputFilename : "obfuscated_" + userConfig.environment.inputFilename;
 
-var dbSetting = userConfig.dbConfig;
+
+//TODO: const input
+function obfuscate(input) {
+    var ouput = input;
+    fs.writeFileSync(outputFilename,input)
+}
+
 function sqlSymmetricObfuscator() {
     console.log(`Loading postgres version ${sqlDialect.version}, dictionary contains ${sqlDialect.keywords.length} words`);
-    console.log("Input and output words" + inputOutputWords.inputWords + inputOutputWords.outputWords );
+    console.log("Input and output words " + inputOutputWords.inputWords + " "+ inputOutputWords.outputWords );
     console.log('obfuscation started...');
-    console.log("in " + inputFilename);
-    console.log("out " + outputFilename);
+
+    console.log("out file name: " + outputFilename);
+
+    fs.readFile(path.resolve(__dirname, inputFilename), {encoding: 'utf8'}, function (err, data) {
+        if (err) throw err;
+        console.log("in " + inputFilename );
+        obfuscate(data);
+        console.log(`+ File ${inputFilename} obfuscated successfuly`);
+    });
 
 }
 
