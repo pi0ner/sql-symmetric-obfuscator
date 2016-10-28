822f32df2c4ff70f2204ba9d152565ccbd2e71ec
pi0ner <pi0ner@inbox.ru>
2016-10-29 00:08:59 +0300

    [#1][+]Config, db.

--- index.js
+++ index.js
@@ -2,11 +2,26 @@
 
  */
 
+var pg = require("pg");
+var fs = require("fs");
+
+var path = require("path");
+
+//TODO: refactoring(много слов)
+var migrationConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'obfuscatorConfig.json'), 'utf8')).migration;
+
+var inputOutputWords = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf8')).inputOutputWords;
+
+var dbSetting = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf8')).dbConfig;
+
 function sqlDeobfuscation() {
     console.log('deobfuscation started...');
+    console.log("Input and output words\n" + inputOutputWords.inputWords +"\n" +  inputOutputWords.outputWords );
 }
 
 function sqlSymmetricObfuscator() {
+
+    console.log("Input and output words" + inputOutputWords.inputWords + inputOutputWords.outputWords );
     console.log('obfuscation started...');
 }
 
